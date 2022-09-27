471298d5d6498afc9a417f85a4d38d841b935329
Roman Montoya <59694362+Roman4u@users.noreply.github.com>
2022-09-27 15:18:03 +0900

    create routes for each controller

---
+++ server/src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { getAllTodos, createTodo, updateTodo, deleteTodo } from "../controllers/Todo";
+
+const router: Router = Router();
+
+router.get("/get", getAllTodos);
+router.post("/add", createTodo);
+router.put("/update/:id", updateTodo);
+router.delete("/delete/:id", deleteTodo);
