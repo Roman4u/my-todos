4cafa9e5c3713890a86e5e1e5be5a56503d2bb0f
Roman Montoya <59694362+Roman4u@users.noreply.github.com>
2022-09-27 15:12:26 +0900

    a controller to retrieve all documents

--- server/src/controllers/Todo.ts
+++ server/src/controllers/Todo.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from "express";
-import mongoose from "mongoose";
 
 import Todo, { ITodo } from "../models/Todo";
 
 const createTodo = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 
   try {
+    // make sure at least one of these keys exist on the returning data object
     const body = req.body as Pick<ITodo, "name" | "description" | "status">;
 
     const todo: ITodo = new Todo({
@@ -15,6 +15,8 @@ const createTodo = async (req: Request, res: Response, next: NextFunction): Prom
     });
 
     const newTodo: ITodo = await todo.save();
+
+    // use the mongoose model to make a query to the DB
     const allTodos: ITodo[] = await Todo.find();
 
     res.status(201).json({ message: "Todo added", todo: newTodo, todos: allTodos })
@@ -23,7 +25,16 @@ const createTodo = async (req: Request, res: Response, next: NextFunction): Prom
   }
 };
 
-const getAuthor = (req: Request, res: Response, next: NextFunction) => {};
-const getAllAuthors = (req: Request, res: Response, next: NextFunction) => {};
-const updateAuthor = (req: Request, res: Response, next: NextFunction) => {};
-const deleteAuthor = (req: Request, res: Response, next: NextFunction) => {};
+const getAllTodos = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try{
+    const allTodos: ITodo[] = await Todo.find();
+    res.status(200).json({ allTodos });
+  }catch(error){
+    console.error(error);
+  }
+};
+
+const updateTodo = (req: Request, res: Response, next: NextFunction) => {};
+const deleteTodo = (req: Request, res: Response, next: NextFunction) => {};
+ 
+export { createTodo, getAllTodos, updateTodo, deleteTodo };
