599e392b64d8b67add485365b274c7d3a6419758
Roman Montoya <59694362+Roman4u@users.noreply.github.com>
2022-09-30 16:59:18 +0900

    created getTodos api call

--- frontend/src/API.ts
+++ frontend/src/API.ts
@@ -2,7 +2,15 @@ import axios, { AxiosResponse } from "axios";
 
 const baseUrl = process.env.REACT_APP_BASE_URL;
 
-export const getTodos = async (): Promise<AxiosResponse> => {}
-export const addTodo = async (): Promise<AxiosResponse> => {}
-export const updateTodo = async (): Promise<AxiosResponse> => {}
-export const deleteTodo = async (): Promise<AxiosResponse> => {}
+export const getTodos = async (): Promise<AxiosResponse<ApiDataType> | void> => {
+    try{
+        const todos: AxiosResponse<ApiDataType> = await axios.get(`${baseUrl}/todos/get`);
+        return todos; 
+    }catch(error){
+        console.error(error);
+    }
+};
+
+export const addTodo = async (): Promise<AxiosResponse | void> => {}
+export const updateTodo = async (): Promise<AxiosResponse | void> => {}
+export const deleteTodo = async (): Promise<AxiosResponse | void> => {}
