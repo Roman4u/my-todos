6c72d9d99bc35eda9d80dbfd0149c0a9efd06ad4
Roman Montoya <59694362+Roman4u@users.noreply.github.com>
2022-09-30 16:39:29 +0900

    make API.ts file to hold all api call

---
+++ frontend/src/API.ts
@@ -0,0 +1,8 @@
+import axios, { AxiosResponse } from "axios";
+
+const baseUrl = process.env.REACT_APP_BASE_URL;
+
+export const getTodos = async (): Promise<AxiosResponse> => {}
+export const addTodo = async (): Promise<AxiosResponse> => {}
+export const updateTodo = async (): Promise<AxiosResponse> => {}
+export const deleteTodo = async (): Promise<AxiosResponse> => {}
