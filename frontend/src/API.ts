a5107f9258cefd4356ccdf1d55a3948d3ac5fafa
Roman Montoya <59694362+Roman4u@users.noreply.github.com>
2022-10-02 11:13:55 +0900

    created updateTodo and deleteTodo api calls

--- frontend/src/API.ts
+++ frontend/src/API.ts
@@ -2,29 +2,58 @@ import axios, { AxiosResponse } from "axios";
 
 const baseUrl = process.env.REACT_APP_BASE_URL;
 
-export const getTodos = async (): Promise<AxiosResponse<ApiDataType> | void> => {
+export const getTodos =
+  async (): Promise<AxiosResponse<ApiDataType> | void> => {
+    try {
+      const todos: AxiosResponse<ApiDataType> = await axios.get(
+        `${baseUrl}/todos/get`
+      );
+      return todos;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+export const addTodo = async (
+  formData: ITodo
+): Promise<AxiosResponse<ApiDataType> | void> => {
+  try {
+    const todo: Omit<ITodo, "_id"> = {
+      name: formData.name,
+      description: formData.description,
+      status: false,
+    };
+
+    const addedTodo: AxiosResponse<ApiDataType> = await axios.post(
+      `${baseUrl}/todos/add`,
+      todo
+    );
+    return addedTodo;
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const updateTodo = async (
+  todo: ITodo
+): Promise<AxiosResponse | void> => {
+  try {
+    const todoUpdate: Pick<ITodo, "status"> = {
+        status: true
+    };
+
+    const updatedTodo: AxiosResponse = await axios.patch(`${baseUrl}/todos/update/${todo._id}`,
+    todoUpdate);
+    return updatedTodo; 
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deleteTodo = async ( _id: string ): Promise<AxiosResponse | void> => {
     try{
-        const todos: AxiosResponse<ApiDataType> = await axios.get(`${baseUrl}/todos/get`);
-        return todos; 
+        const deletedTodo: AxiosResponse = await axios.delete(`${baseUrl}/todos/delete/${_id}`);
+        return deletedTodo;
     }catch(error){
         console.error(error);
     }
 };
-
-export const addTodo = async ( formData: ITodo ): Promise<AxiosResponse<ApiDataType> | void> => {
-    try{
-        const todo: Omit<ITodo, "_id"> = {
-            name: formData.name,
-            description: formData.description,
-            status: false
-        };
-
-        const addedTodo: AxiosResponse<ApiDataType> = await axios.post(`${baseUrl}/todos/add`, todo);
-        return addedTodo; 
-        
-    }catch(error){
-        console.error(error);
-    }
-}
-export const updateTodo = async (): Promise<AxiosResponse | void> => {}
-export const deleteTodo = async (): Promise<AxiosResponse | void> => {}
