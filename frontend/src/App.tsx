6e2bdfe4a5b06864716b0e53f21e39baf5f5b966
Roman Montoya <59694362+Roman4u@users.noreply.github.com>
2022-10-03 11:02:46 +0900

    created method to handle updating user input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,58 @@
 import React, { useEffect, useState } from "react";
-import { addTodo, getTodos } from "./API";
+
+import TodoItem from "./components/TodoItem";
+import { addTodo, getTodos, updateTodo } from "./API";
 import AddTodo from "./components/AddTodo";
 
 function App() {
-
   const [todos, setTodos] = useState<ITodo[]>([]);
 
   const fetchTodos = async (): Promise<any> => {
-    try{
+    try {
       const data = await getTodos();
-      const todoList: ITodo[] = data.todos; 
+      const todoList: ITodo[] = data.todos;
       setTodos(todoList);
-    }catch(error){
+    } catch (error) {
       console.error(error);
     }
-  } 
+  };
 
+  // this functions takes user input and sets the state
+  // with the data returned from the addTodo api call
   const handleSaveTodo = async (
     e: React.FormEvent,
     formData: ITodo
   ): Promise<any> => {
     e.preventDefault();
 
-    try{
+    try {
       const data = await addTodo(formData);
-      console.log("data:", data);
-      console.log("data.data:", data.data);
-    }catch(error){
+      if (data.status !== 201) {
+        throw new Error("Error! No Todo Saved");
+      }
+      setTodos(data.todos);
+    } catch (error) {
       console.error(error);
     }
-  
+  };
+
+  const handleUpdateTodo = async (todo: ITodo): Promise<any> => {
+    try {
+      const data = await updateTodo(todo);
+      if (data.status !== 201) {
+        throw new Error("Error! Todo not updated");
+      }
+      setTodos(data.todos);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDeleteTodo = (_id: string): void => {
+    try {
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   useEffect(() => {
@@ -40,6 +63,14 @@ function App() {
     <main className="App">
       <h1>My List of Todos</h1>
       <AddTodo saveTodo={handleSaveTodo} />
+      {todos.map((todo: ITodo) => {
+        <TodoItem
+          key={todo._id}
+          updateTodo={handleUpdateTodo}
+          deleteTodo={handleDeleteTodo}
+          todo={todo}
+        />;
+      })}
     </main>
   );
 }
